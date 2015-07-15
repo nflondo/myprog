595bb3b9266b85c617d22db5215a9d9211ca0b35
Nefi Munoz <nmunoz@xmission.com>
2015-07-15 05:59:40 -0600

    added infinite loop

--- python/while-input.py
+++ python/while-input.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # Check user input
 
-age = raw_input("Give me your age in years (e.g. 34): ")
-while not age.isdigit():
-    print "I'm sorry but {} isn't valid.".format(age)
-    age = raw_input("Give me your age in years (e.g. 34): ")
-print "Thanks.  Your age is set to {}".format(age)
+while True:
+    age = raw_input("Give me your age in years (e.g. 34): Enter 'q' to quit: ")
+    if age == 'q':
+        print "Good Bye"
+        break
+    while not age.isdigit():
+        print "I'm sorry but {0} isn't valid.".format(age)
+        age = raw_input("Give me your age in years (e.g. 34): ")
+    print "Thanks.  Your age is set to {0}".format(age)
