76e3e3f925838b70320471d9a20cca0ae484e1bf
Nefi Munoz <nmunoz@suse.com>
2015-07-16 16:53:22 -0600

    Added stuff

--- python/input.py
+++ python/input.py
@@ -6,16 +6,20 @@ lunch_notes="with teriyaki sause"
 dinner_special="buffalo steak"
 dinner_notes="top loin"
 
-meal_time=raw_input('Which mealtime do you want? [breakfast, lunch, dinner]')
-print 'Specials for {0}:'.format(meal_time)
-if meal_time == 'breakfast':
-    print breakfast_special
-    print breakfast_notes
-elif meal_time == 'lunch':
-    print lunch_special
-    print lunch_notes
-elif meal_time == 'dinner':
-    print dinner_special
-    print dinner_notes
-else:
-    print 'Sorry, {0} isn\'t valid.'.format(meal_time)
+while True:
+    meal_time=raw_input("Which mealtime do you want? [breakfast, lunch, dinner, 'q' to quit]: ")
+    if meal_time == 'q':
+        print "Good Bye"
+        break
+    print 'Specials for {0}:'.format(meal_time)
+    if meal_time == 'breakfast':
+        print breakfast_special
+        print breakfast_notes
+    elif meal_time == 'lunch':
+        print lunch_special
+        print lunch_notes
+    elif meal_time == 'dinner':
+        print dinner_special
+        print dinner_notes
+    else:
+        print 'Sorry, {0} isn\'t valid.'.format(meal_time)
