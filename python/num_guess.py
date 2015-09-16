75adc1f0564cf8a0be2868b0a4aecca268d5c6f0
Nefi Munoz <nmunoz@suse.com>
2015-09-16 16:48:02 -0600

    Fixed (int) assignment; now is a working program

--- python/num_guess.py
+++ python/num_guess.py
@@ -2,31 +2,34 @@
 # number-guessing game
 
 import random 
-import pdb
+#import pdb
+guessesTaken=0
 secret_num = random.randint(1,10) 
 print "Secret number: {0}".format(secret_num)
 print "I have a number..."
 
-while True:
-    guess = raw_input("What is your guess [1-10]?: Enter 'q' to quit: ")
-    guess=guess.strip()
-    int(guess)
-    if guess == 'q':
-        print "Good Bye"
-        break
-    while not guess.isdigit():
-        print "I'm sorry but {0} isn't valid.".format(guess)
-        guess = raw_input("What is your guess [1-10]?: Enter 'q' to quit: ")
-        int(guess)
-    #print "Thanks.  Your age is set to {0}".format(age)
-    pdb.set_trace() 
-    if guess > secret_num:
-        print "That's too high.  Try again !"
-    elif guess < secret_num:
-        print "That's too low.  Try again !"
-    else:
-        print "You got it !! Thanks for playing !!"
-        #return 
+#while True:
+while guessesTaken < 3:
+	guess = raw_input("What is your guess [1-10]?: Enter '0' to quit: ")
+	print "My guess: {0}".format(guess)
+	while not guess.isdigit():
+		print "I'm sorry but \"{0}\" isn't valid.".format(guess)
+		guess = raw_input("What is your guess [1-10]?: Enter '0' to quit: ")
+	
+	guess = int(guess)
+	guessesTaken = guessesTaken +1
+	if guess == 0:
+		print "Good Bye"
+		break
+	
+	#pdb.set_trace() 
+	if guess > secret_num:
+		print "That's too high.  Try again !"
+	elif guess < secret_num:
+		print "That's too low.  Try again !"
+	else:
+		print "You got it !! Thanks for playing !!"
+		break 
 
 
 
