a23caf0cbbc1f5c9f02c90a5a6db15f28a34629a
Nefi Munoz <nmunoz@suse.com>
2015-09-23 16:55:00 -0600

    bunch of changes

--- python/restaurant_project/classes/inventory.py
+++ python/restaurant_project/classes/inventory.py
@@ -6,24 +6,26 @@ class Inventory( object):
 		self.items = items 
 
 	def add( self, item): 
-		if self.items.haskey( item): 
+		if self.items.has_key( item): 
 			self.items[ item] += 1 
 		else: 
 			self.items[ item] = 1 
 
 	def remove( self, item): 
-		if self.items.haskey( item): 
+		if self.items.has_key( item): 
 			if self.items[ item] < 1: 
 				print "Can't go negative. Sorry" 
 			else: 
 				self.items[ item] -= 1 
 
 	def check( self, item): 
-		if self.items.haskey( item): 
+		if self.items.has_key(item): 
+            print "Item: {0} is on inventory".format(item)
 		    return True 
 		else: 
+            print "Item: {0} is NOT on inventory".format(item)
 		    return False 
 
-	def print_inventory( self): 
+	def print_inventory(self): 
 		for item in self.items: 
 		    print item, "-", self.items[item]
--- python/restaurant_project/main.py
+++ python/restaurant_project/main.py
@@ -7,17 +7,22 @@ from classes.inventory import Inventory
 
 def main(): 
 	i = Ingredient( title =" egg") 
-
 	r = Recipe( title ="Scrambled eggs", ingredients =[ i], directions =['Break egg', 'Beat egg', 'Cook egg']) 
-
 	r.print_recipe()
 	
 	oats = Ingredient(title = "Oats")
 	oatmeal = Recipe(title = "Oatmeal", ingredients = [oats], directions =['cook oatmeal in a pan', 'serve oatmeal'])
 	oatmeal.print_recipe()
 	
-	item1 = Inventory(items = "salt")
-	item1.check()
+	item1 = Inventory(items = {'salt':2, 'eggs':5})
+	print "Before adding:"
+	item1.print_inventory()	
+	item1.add(item = "eggs")
+	print "After adding:"
+	item1.print_inventory()	
+	
+	item1.check(item="salt")
+	item1.check(item="onions")
 	 
 if __name__ == "__main__": 
 	main()
