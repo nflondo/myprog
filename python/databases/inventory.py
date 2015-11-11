ca9db8bd22f87e4408361ca6950f4113334d4686
Nefi Munoz <nmunoz@suse.com>
2015-11-11 16:20:14 -0700

    Added search capability

--- python/databases/inventory.py
+++ python/databases/inventory.py
@@ -29,11 +29,32 @@ def add_ingredients(cursor):
             print "OK, quitting."
             break
 
+def print_inventory(cursor):
+    sql = '''select * from ingredients'''
+    results = cursor.execute(sql)
+    all_results = results.fetchall()
+    for item in all_results:
+        print item
+
+def search_item(cursor):
+    findme = raw_input("Item to search: ")
+    sql = '''select title,amount from ingredients where title like :searchme'''
+    results = cursor.execute(sql, {"searchme":findme})
+    all_results = results.fetchall()
+    if all_results:
+        for item in all_results:
+            print item
+    else:
+        print "Item:",findme,"not found."
+            
+
 def main():
     conn = open_database('inventory.db')
     cursor = conn.cursor()
     create_table(cursor)
-    add_ingredients(cursor)
+    #add_ingredients(cursor)
+    print_inventory(cursor)
+    search_item(cursor)
     conn.commit()
     conn.close()
 
