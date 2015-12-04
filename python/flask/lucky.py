4867be295c321d13875e452e03f86315a504a5c9
Nefi Munoz <nmunoz@xmission.com>
2015-12-04 05:59:03 -0700

    Added Jinja code

--- python/flask/lucky.py
+++ python/flask/lucky.py
@@ -2,13 +2,15 @@
 # 
 from flask import Flask
 from flask import render_template
+from random import randint
 
 # This sets up a flask instance and calls it 'app'
 app = Flask(__name__)
 
 @app.route('/')
 def lucky():
-    return render_template('lucky.html')
+    lucky_num = randint(1,10)
+    return render_template('lucky.html', lucky_num=lucky_num)
 
 if __name__=='__main__':
     app.run()
