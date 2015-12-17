58c1d03ad2f86295c4830b6647658f28ec6ae72c
Nefi Munoz <nmunoz@suse.com>
2015-12-17 16:29:09 -0700

    Added dynamic special based on

--- python/flask/restaurant.py
+++ python/flask/restaurant.py
@@ -3,13 +3,29 @@
 from flask import Flask
 from flask import render_template
 import json
+from datetime import datetime
+
+
 
 # This sets up a flask instance and calls it 'app'
 app = Flask(__name__)
 
+def get_specials(filename):
+    f = open(filename)
+    specials = json.load(f)
+    f.close()
+    return specials
+
+def get_special():
+    specials = get_specials('specials.json')
+    today = datetime.now()
+    DAYS_OF_WEEK = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
+    weekday = DAYS_OF_WEEK[today.weekday()]
+    return specials[weekday]
+   
 @app.route('/')
 def main_page():
-    return render_template('base.html', special="Hamburger")
+    return render_template('base.html', special= get_special())
 
 if __name__=='__main__':
     app.run()
