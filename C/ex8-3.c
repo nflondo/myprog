cc6de468ef86afd329faed07a3810527a34af72e
nm75 <nmunoz@suse.com>
2016-04-27 16:41:20 -0600

    changed permissions

--- C/ex8-3.c
+++ C/ex8-3.c
@@ -18,19 +18,21 @@ float f_array[MAX] = { .0, .1, .2, .3, .4, .5, .6, .7, .8, .9 };
 
 /* Declare a pointer to float. */
 
-double *f_ptr;
+float *f_ptr;
 
 int main(void)
 {
      /* Initialize the pointers. */
 
-     i_ptr = i_array;
-     f_ptr = f_array;
+     i_ptr=i_array;
+     f_ptr=f_array;
 
      /* Print the array elements. */
 
-     for (count = 0; count < MAX; count++)
-	 printf("%d\t%f\n", i_ptr++, f_ptr++);
+     for (count = 0; count < MAX; count++){
+	 	printf("%d\t%f\n", *i_ptr++, *f_ptr++);
+
+	}
 
      return 0;
 }
