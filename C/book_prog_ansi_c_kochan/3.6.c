d48b61ecbd27f8653515f6ddf9dc1b61d0565f56
nm75 <nmunoz@suse.com>
2016-05-10 19:43:17 -0600

    initial files

---
+++ C/book_prog_ansi_c_kochan/3.6.c
@@ -0,0 +1,16 @@
+/*Adds two integer values*/
+#include <stdio.h>
+
+main()
+{
+    //Declare variables
+    int value1, value2, sum;
+    
+    //Assign values and compute the result
+    value1=50;
+    value2=25;
+    sum = value1 + value2;
+    
+    printf("The sum of %i and %i is %i\n", value1, value2, sum);
+
+}
