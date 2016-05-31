fe3f144647e5c71afb2e13749f5e311150827a02
nm75 <nmunoz@suse.com>
2016-05-31 16:39:31 -0600

    New file

---
+++ C/book_prog_ansi_c_kochan/8.12.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/* Sort an array of int into ascending order*/
+
+void sort(int a[], int n){
+	int i,j,temp;
+
+	for (i=0; i < n - 1; ++i)
+		for (j = i + 1; j < n; ++j)
+			if (a[i] > a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+	
+} // sort
+
+main(){
+	int i;
+	int array[16] = { 34, -5, 6, 0, 12, 100, 56, 22, 44, -3, -9, 12, 17, 22, 6, 11 };
+
+	printf ("The array before the sort:\n");
+	for (i =0; i< 16; ++i )
+		printf("%i ", array[i]);
+
+	sort (array, 16);
+
+	printf ("\n\nThe array after the sort:\n");
+
+	for (i =0; i< 16; ++i )
+		printf("%i ", array[i]);
+
+	printf ("\n");
+
+}
