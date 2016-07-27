7da20b8528d13b7b7e64b786d9bb31c87415feaa
nm75 <nmunoz@suse.com>
2016-07-27 06:05:25 -0600

    Added entire 10.9 program

--- C/book_prog_ansi_c_kochan/10.9.c
+++ C/book_prog_ansi_c_kochan/10.9.c
@@ -11,10 +11,10 @@ struct entry{
 int equal_strings (char s1[], char s2[]){
     int i=0, answer;
     
-    while ( s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' )
+    while ( s1[i]==s2[i] && s1[i] != '\0' && s2[i] != '\0' )
         ++i;
     
-    if ( s1[i] == '\0' && s2[i] = '\0' )
+    if ( s1[i] == '\0' && s2[i] == '\0' )
         answer = 1; // Strings equal
     else
         answer = 0; // not equal
@@ -22,3 +22,40 @@ int equal_strings (char s1[], char s2[]){
     return (answer);
 
 } // equal strings
+
+/*Look up a word in a dictionary*/
+int lookup (struct entry dictionary[], char search[], int entries){
+    //int equal_strings (char s1[], char s2[]);
+    int i;
+    
+    for (i=0; i < entries; ++i)
+        if ( equal_strings (search, dictionary[i].word) )
+            return (i);
+            
+    return (-1);
+
+} // lookup
+
+main(){
+
+    struct entry dictionary[10] =
+    {{ "aardvark", "a burrowing African mammal"     },
+     { "abyss", "a bottomless pit"                  },
+     { "acumen", "mentally sharp; keen"             }};   
+    
+    char word[10];
+    int entries = 3;
+    int entry_number;
+    //int lookup (struct entry dictionary[], char search[], int entries);
+
+    printf("Enter word: ");
+    scanf("%9s", word);
+    
+    entry_number = lookup (dictionary, word, entries);
+    
+    if ( entry_number != -1 )
+        printf ("%s\n", dictionary[entry_number].definition);
+    else
+        printf ("Sorry, that word is not in my dictionary.\n");
+
+} // main
