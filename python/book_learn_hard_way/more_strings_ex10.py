5026d3c55c74b3b2db152ef5c67364f44472d939
nm75 <nmunoz@suse.com>
2016-11-02 06:02:09 -0600

    new files

---
+++ python/book_learn_hard_way/more_strings_ex10.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+fat_cat2 = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print fat_cat2
+
+
+                                                                                       
+while True:
+    for i in ['/','-','|','\\','|']:
+        print "%s\r" % i,
+        
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i,
