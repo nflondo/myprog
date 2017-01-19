a368b63bbaaaff6b18cff4064a023bc146fe56a0
nm75 <nmunoz@suse.com>
2017-01-19 05:52:08 -0700

    changes

--- Java/21days/ecommerce/src/com/prefect/ecommerce/Storefront.java
+++ Java/21days/ecommerce/src/com/prefect/ecommerce/Storefront.java
@@ -3,5 +3,27 @@ package com.prefect.ecommerce;
 import java.util.*;
 
 public class Storefront {
+	private LinkedList catalog = new LinkedList();
 	
+	public void addItem(String id, String name, String price, String quant){
+		
+		Item it = new Item(id, name, price, quant);
+		catalog.add(it);
+	}
+	
+	public Item getItem(int i){
+		return (Item)catalog.get(i);
+		
+	}
+	
+	public int getSize(){
+		return catalog.size();
+	}
+
+	//Sorts a linked list and other data structures based on the natural sort order
+	//of the objects they contain, calling the object's compareTo() method to determine
+	//this order.
+	public void sort(){
+		Collections.sort(catalog);
+	}
 }
