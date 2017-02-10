ba01bbd8d01617d168c3b9f0d83f95c41b16a057
nm75 <nmunoz@suse.com>
2017-02-10 05:52:26 -0700

    Added comments

--- Java/21days/ecommerce/src/com/prefect/ecommerce/Storefront.java
+++ Java/21days/ecommerce/src/com/prefect/ecommerce/Storefront.java
@@ -1,6 +1,10 @@
 package com.prefect.ecommerce;
-
 import java.util.*;
+
+/*
+ * Manages a collection of products in an online store.  Each product is an Item object, and they are stored
+ * in a LinkedList instance variable called "catalog"
+ */
 
 public class Storefront {
 	private LinkedList catalog = new LinkedList();
@@ -8,7 +12,7 @@ public class Storefront {
 	public void addItem(String id, String name, String price, String quant){
 		
 		Item it = new Item(id, name, price, quant);
-		catalog.add(it);
+		catalog.add(it);  // linkedlist add method
 	}
 	
 	public Item getItem(int i){
@@ -16,12 +20,13 @@ public class Storefront {
 		
 	}
 	
+	// Returns number of objects in the catalog
 	public int getSize(){
 		return catalog.size();
 	}
 
-	//Sorts a linked list and other data structures based on the natural sort order
-	//of the objects they contain, calling the object's compareTo() method to determine
+	//The Class Method Collections.sort sorts a linked list and other data structures based on the natural 
+	//sort order of the objects they contain, calling the object's compareTo() method to determine
 	//this order.
 	public void sort(){
 		Collections.sort(catalog);
