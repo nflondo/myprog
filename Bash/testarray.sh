240d80f8ad8bd77956736eb22a5b139ae591cfa1
nm75 <nmunoz@suse.com>
2017-03-20 16:23:09 -0600

    changes

--- Bash/testarray.sh
+++ Bash/testarray.sh
@@ -8,3 +8,14 @@ echo ${atest[@]}
 
 echo "array size: "
 echo ${#atest[*]}
+
+/home/myprog/Bash/kdump-mem
+rc=$?
+
+if [ $rc -eq 0 ]; then
+	echo "testing passed"
+	exit 0
+else
+	echo "testing failed"
+	exit 1
+fi
