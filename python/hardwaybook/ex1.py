46c929eb4bbf61402cb30e728e472a4296ea380d
nm75 <nmunoz@suse.com>
2017-06-12 05:57:37 -0600

    new dir

---
+++ python/hardwaybook/ex1.py
@@ -0,0 +1,7 @@
+print "Hello world!"
+print "Hello again"
+print "I like typing this."
+print "this is fun."
+print 'yay! printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
