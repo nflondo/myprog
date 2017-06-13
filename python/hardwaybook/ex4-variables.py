c246920c59393f5e969d63459593b8e17783c111
Nefi Munoz <nefi.munoz@gmail.com>
2017-06-13 08:07:47 -0600

    new file

---
+++ python/hardwaybook/ex4-variables.py
@@ -0,0 +1,18 @@
+# Notice space in variable assignments.  Works without space too, but looks ugly.
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+#Note there is no need to add space inside Strings for the variable
+#space is automatically added
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
