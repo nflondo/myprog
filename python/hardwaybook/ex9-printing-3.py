294a0efa42f4d6ad74034c2e83639c14daba34e4
Nefi Munoz <nefi.munoz@gmail.com>
2017-06-14 16:35:12 -0600

    new files

---
+++ python/hardwaybook/ex9-printing-3.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# Here is a multi-line comment
+print """
+There's something  going on here.
+With the three double-quotes.
+we'll be able to type as much as we like
+"""
