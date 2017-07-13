aa2bb8718b590dd6f00a43bc5d54d9516a8ad0fd
Nefi Munoz <nefi.munoz@gmail.com>
2017-07-13 16:40:58 -0600

    added

--- python/hardwaybook/ex31-decisions.py
+++ python/hardwaybook/ex31-decisions.py
@@ -16,4 +16,18 @@ if door == "1":
     else:
         print "Well, doing %s is probably better.  Bear runs away." % bear
         
-                
+elif door == "2":
+    print "You stare into the endless abyss at Cthulhu's retina."
+    print "1. Blueberries."                
+    print "2. Yellow jacket clothespins."
+    print "3. Understanding evolvers yelling melodies."
+    
+    insanity = raw_input("> ")
+    
+    if insanity == "1" or insanity == "2":
+        print "Your body survives poered by a mind of jello.  Good job!"
+    else:
+        print "The insanity rots your eyes into a pool of muck. Good job!"
+        
+else:
+    print "You stumble around and fall on a knife and die.  Good job!"                
