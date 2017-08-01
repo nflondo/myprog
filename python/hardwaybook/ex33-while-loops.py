5669457753f57753f8691a0ec6d12308c695f17a
Nefi Munoz <nefi.munoz@gmail.com>
2017-08-01 16:22:13 -0600

    changes

--- python/hardwaybook/ex33-while-loops.py
+++ python/hardwaybook/ex33-while-loops.py
@@ -1,23 +1,22 @@
 
-def fill_list():
+def fill_list(end, increment):
     i = 0
     numbers = []
-    end = 6
+#    end = 6
     while i < end:
         print "At the top i is %d" % i
         numbers.append(i)
         
-        i = i + 1
+        i = i + increment
         print "Numbers now: ", numbers
         print "At the bottom i is %d" %i
     
     #print "The numbers: "
     return numbers
 
-#    for num in numbers:
-#        print num    
- 
-fill_list() 
-#mylist=fill_list()
-#print "My list: %r" % mylist
+
+mylist=fill_list(4, 1)
+print "My list: %r" % mylist
         
+mylist=fill_list(6, 2)
+print "My list: %r" % mylist        
