db8e6e0d6b2f24d2684271a15874e2c6be0f28b2
Nefi Munoz <nefi.munoz@gmail.com>
2017-09-25 15:01:09 -0600

    changes

---
+++ python/hardwaybook/frightmares.py
@@ -0,0 +1,49 @@
+# You go to several rooms.  You have to answer a question in each room.  If you
+# answer correctly, you get a candy.  If you don't, you get a penalty.
+# Pirate room: What does a pirate say? (arrg) Penalty: You lose an eye
+# Day of the dead room: How do you say dead in spanish? (muerto) Penalty: You 
+# wait 5 min
+# 60's room: Can you dance? (dance) Penalty: you get a kick in the butt
+# Race car room: Run fast, did you win? Penalty: You get a pinch
+
+
+
+from sys import exit
+
+class Room(object):
+
+    def enter(self):
+        pass
+        
+class Pirate(Room):
+
+    def enter(self):
+        pass
+
+class DayOfDead(Room): 
+
+    def enter(self):
+        pass
+
+class Dance(Room): 
+
+    def enter(self):
+        pass
+
+class Race(Room):
+
+    def enter(self):
+        pass
+
+     Child 
+
+        walks 
+
+
+     
+
+    Attendant 
+
+        quiz 
+
+    Main
