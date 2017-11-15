6e8dd11ed4ee2d43e5265e9eff6ecd3efda5263c
Nefi Munoz <nefi.munoz@gmail.com>
2017-11-15 16:39:03 -0700

    Removed the Object oriented stuff, not needed

--- python/hardwaybook/projects/ex48-proj/tests/lexicon_tests.py
+++ python/hardwaybook/projects/ex48-proj/tests/lexicon_tests.py
@@ -1,11 +1,8 @@
 from nose.tools import *
-from ex48.lexicon import Lexicon
-#import lexicon
-#import ex48.lexicon.scan
+from ex48 import lexicon
 
 
 def test_directions():
-    lexicon = Lexicon()
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
     result = lexicon.scan("north south east")
     assert_equal(result, [('direction', 'north'),
@@ -13,7 +10,6 @@ def test_directions():
                             ('direction', 'east')])
 
 def test_verbs():
-    lexicon = Lexicon()
     assert_equal(lexicon.scan("go"), [('verb', 'go')])
     result = lexicon.scan("go kill eat")
     assert_equal(result, [('verb', 'go'),
