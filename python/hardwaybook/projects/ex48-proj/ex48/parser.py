b50a6c35544866d59f40b82c5e5a63990b3f4086
Nefi Munoz <nefi.munoz@gmail.com>
2017-12-15 16:39:08 -0700

    parser.py: * finished copying the file

--- python/hardwaybook/projects/ex48-proj/ex48/parser.py
+++ python/hardwaybook/projects/ex48-proj/ex48/parser.py
@@ -12,6 +12,7 @@ class Sentence(object):
             
 
 def peek(word_list):
+    '''Look at next element in our tuple list, and then match to take one off'''
     if word_list:
         word = word_list[0]
         return word[0]
@@ -42,4 +43,35 @@ def parse_verb(word_list):
     else:
         raise ParserError("Expected a verb next.")
         
-def parse_object                                                                    
+def parse_object (word_list):
+    skip(word_list, 'stop')
+    next = peek(word_list)
+    
+    if next == 'noun':
+        return match(word_list, 'noun')
+    if next == 'direction':
+        return match(word_list, 'direction')
+    else:
+        raise ParserError("Expected a noun or direction next.") 
+        
+def parse_subject(word_list, sub):
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)  
+    
+    return Sentence(subj, verb, obj)
+    
+def parse_sentence(word_list):
+    skip(word_list, 'stop')
+    
+    start = peek(word_list)
+    
+    if start == 'noun':
+        subj = match(word_list, 'noun')
+        return parse_subject(word_list, subj)
+    elif start == 'verb':
+        # assume the subject is the player then
+        return parse_subject(word_list, ('noun', 'player'))  
+    else:
+        raise ParserError("Must start with subject, object, or verb not: %s" %start)          
+    
+                                                                                     
