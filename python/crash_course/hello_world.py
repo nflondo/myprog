24c3758343fce365153abec420ba6b52c01049ac
Nefi Munoz <nefi.munoz@gmail.com>
2018-01-17 16:42:32 -0700

    new files

---
+++ python/crash_course/hello_world.py
@@ -0,0 +1,13 @@
+message = "Hello world from Geany"
+print (message);
+
+name = "aDa loveLace"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+# String concatenation
+first_name = "John"
+last_name = "doe"
+full_name = first_name + " " + last_name
+print("Hello" + " " + full_name.title() + "!")
