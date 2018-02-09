f243b67bc1edd5b5f7e01fc860d888217974b0f8
Nefi Munoz <nefi.munoz@gmail.com>
2018-02-09 16:18:49 -0700

    changes

--- python/crash_course/ch6/ex-6.py
+++ python/crash_course/ch6/ex-6.py
@@ -26,9 +26,39 @@ for key, value in fav_numbers.items():
 glossary = {
     'dictionary': 'A collection of key-value pairs',
     'list': 'A collection of items in a particular order',
-    'tuple': 'An immutable list'
+    'tuple': 'An immutable list',
+    'key': 'Index in a dictionary',
+    'value': 'The value of the key',
     }
     
 for key, value in glossary.items():
     print("\n" + key.title() + ": " + value)
     
+rivers = {
+    'nile': 'Egipt',
+    'amazon': 'Brazil',
+    'jordan': 'Israel',
+    }
+for key in rivers.keys():
+    print("The " + key + " runs through " + rivers[key].title())
+    
+for river in rivers.keys():
+    print(river)    
+        
+for country in rivers.values():
+    print(country) 
+    
+favorite_languages = {
+        'jen': 'python',
+        'sarah': 'c',
+        'edward': 'ruby',
+        'phil': 'python',
+        }
+
+candidates = ['jim', 'bruce', 'jen','phil', 'ann']
+
+for candidate in candidates:
+    if candidate in favorite_languages.keys():
+        print("Thank you for taking the poll, " + candidate.title())
+    else:
+        print(candidate.title() + ", please take the poll.  Thanks !")
--- python/crash_course/ch6/lecture.py
+++ python/crash_course/ch6/lecture.py
@@ -15,3 +15,46 @@ print(aliens)
 del aliens['points']
 print(aliens)
 
+# Looping through keys in a dictionary with keys() This is default
+# behaviour
+fav_numbers = {
+    'lili': 5,
+    'ben' : 7,
+    'anna' : 25,
+    'sam' : 3,
+    }
+
+friends = ['sam', 'anna']    
+for name in fav_numbers.keys():
+    print(name.title())  
+    if name in friends:
+        print(" Hi " + name.title() + "," + 
+            "I see your favority number is: " + str(fav_numbers[name]))
+
+# keys() actually returns a list of all the keys
+if 'erin' not in fav_numbers.keys():
+    print("Erin, please take our poll !!")
+
+# Looping through keys in order with sorted()
+for name in sorted(fav_numbers.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+    
+# Looping through all values in a dictionary with values()
+print("The following numbers have been mentioned:")
+for num in fav_numbers.values():
+    print(num)
+    
+# To avoid repeating values use set()    
+print("The following numbers have been mentioned:")
+for num in set(fav_numbers.values()):
+    print(num)    
+
+# A list of dictionaries
+alien0 = {'color': 'green', 'points': 5}
+alien1 = {'color': 'yellow', 'points': 10}
+alien2 = {'color': 'red', 'points': 15}
+
+aliens = [alien0, alien1, alien2]
+
+for alien in aliens:
+    print(alien)
