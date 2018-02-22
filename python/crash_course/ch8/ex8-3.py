a98549ac147e1bf0b6fffa2f4c66c60127d3685d
Nefi Munoz <nefi.munoz@gmail.com>
2018-02-22 15:29:41 -0700

    changes

--- python/crash_course/ch8/ex8-3.py
+++ python/crash_course/ch8/ex8-3.py
@@ -1,1 +1,23 @@
-# 8-3 functions
+# 8-3, 8-4 functions
+
+def make_shirt(shirt_size='L', shirt_message='I love Python'):
+    print("Shirt size is: " + shirt_size + " With message: " +
+        shirt_message)
+
+make_shirt()
+make_shirt(shirt_size='M')
+make_shirt(shirt_size='XL', shirt_message='You re never wrong to do' + 
+    ' the rigth thing')
+
+def describe_city(city_name, country='usa'):
+    print(city_name + " is in " + country)
+    
+describe_city('Midvale')    
+describe_city('Villahermosa', 'Mex')
+describe_city(country='Spain', city_name='Barcelona') 
+
+def city_country(city, country):
+    print(city.title() + ", " + country.title())
+    
+city_country('Salt Lake City', 'USA')       
+city_country('Buenos Aires', 'Argentina')       
--- python/crash_course/ch8/lecture-1.py
+++ python/crash_course/ch8/lecture-1.py
@@ -41,5 +41,20 @@ def describe_pet2(pet_name, animal_type='dog'):
 describe_pet2(pet_name='willie')
 #or
 describe_pet2('charlie')
-describe_pet2()
-# Equivalent function calls
+
+
+# Return Values.  Note empty string is FALSE, non-empty is TRUE
+# Optional argument has empty string and is last
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+    
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = get_formatted_name('john', 'hooker', 'lee')    
+print(musician)
