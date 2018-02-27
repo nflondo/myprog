2816d796fe634697f7d554ae8fc5a92979e00753
Nefi Munoz <nefi.munoz@gmail.com>
2018-02-27 05:56:57 -0700

    new files

---
+++ Java/21days/com/java21days/MousePrank.java
@@ -0,0 +1,84 @@
+package com.java21days;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MousePrank extends JFrame implements ActionListener{
+    public MousePrank(){
+        super("Message");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(420,220);
+        BorderLayout border = new BorderLayout();
+        setLayout(border);
+        JLabel msg = new JLabel("Click OK to close program.");
+        add(BorderLayout.NORTH, msg);
+        PrankPanel prank = new PrankPanel();
+        prank.ok.addActionListener(this);
+        add(BorderLayout.CENTER, prank);
+        setVisible(true);
+    }// constructor
+    
+    public void actionPerformed(ActionEvent event){
+        System.exit(0);
+    }
+    
+    public Insets getInsets(){
+        return new Insets(40, 10, 10, 10);
+    }
+    
+    private static void setLookAndFeel(){
+    
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Metal".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Couldn't use the system look and feel: " + e);
+        }
+
+    }
+    
+    public static void main(String[] args){
+        MousePrank.setLookAndFeel();
+        new MousePrank();
+    }
+}// class
+
+class PrankPanel extends JPanel implements MouseMotionListener{
+    JButton ok = new JButton("OK");
+    int buttonX, buttonY, mouseX, mouseY;
+    int width, height;
+    
+    PrankPanel(){
+        super();
+        setLayout(null);
+        addMouseMotionListener(this);
+        buttonX = 110;
+        buttonY = 110;
+        ok.setBounds(new Rectangle(buttonX, buttonY, 70, 20));
+        add(ok);
+    }
+    
+    public void mouseMoved(MouseEvent event){
+        mouseX = event.getX();
+        mouseY = event.getY();
+        width = (int) getSize().getWidth();
+        height = (int) getSize().getHeight();
+        if (Math.abs ((mouseX + 35) - buttonX) < 50){
+            buttonX = moveButton(mouseX, buttonX, width);
+            repaint();
+        }
+        if (Math.abs((mouseY + 10) - buttonY) < 50){
+            buttonY = moveButton(mouseY, buttonY, height);
+            repaint();
+        }
+    }
+    
+    public void mouseDragged(MouseEvent event){
+        // ignore this event
+    }
+}
---
+++ Java/21days/com/java21days/Variables.java
@@ -0,0 +1,15 @@
+package com.java21days;
+
+public class Variables{
+
+    public static void main(String[] args){
+        final char UP = 'U';
+        byte initialLevel = 12;
+        short location = 13250;
+        int score = 3500100;
+        boolean newGame = true;
+        
+        System.out.println("Level: " + initialLevel);
+        System.out.println("Up: " + UP);
+    }
+}
