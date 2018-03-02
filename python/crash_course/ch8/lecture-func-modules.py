c750506c8cb30044ea67c784326dadbdd18689e8
Nefi Munoz <nefi.munoz@gmail.com>
2018-03-02 16:07:50 -0700

    changes

--- python/crash_course/ch8/lecture-func-modules.py
+++ python/crash_course/ch8/lecture-func-modules.py
@@ -1,2 +1,12 @@
 # Storing functions in modules
 # Modules
+# See making_pizzas.py and pizza.py
+
+# Import specific functions
+# from module import function_0, function_1, function_2
+# With this syntax you don't need dot notation
+from pizza import make_pizza
+
+make_pizza(10, 'bacon', 'onions', 'jalapenos')
+
+# function alias
