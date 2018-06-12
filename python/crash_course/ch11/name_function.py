d181e57e99c9d847d6707c8ddfe3b2fa62c87649
Nefi Munoz <nefi.munoz@gmail.com>
2018-06-12 16:49:20 -0600

    changes

--- python/crash_course/ch11/name_function.py
+++ python/crash_course/ch11/name_function.py
@@ -1,4 +1,7 @@
-def get_formatted_name(first,last):
+def get_formatted_name(first, last, middle=''):
 	"""Generate a neatly formatted full name"""
-	full_name = first + ' ' + last
+	if middle:
+		full_name = first + ' ' + middle + ' ' + last
+	else:
+		full_name = first + ' ' + last
 	return full_name.title()
