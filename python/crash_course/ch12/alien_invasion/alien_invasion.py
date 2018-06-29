2be5ebb06d06bb23aeacd7639f7425bdf0858cd8
Nefi Munoz <nefi.munoz@gmail.com>
2018-06-29 16:05:13 -0600

    changes

--- python/crash_course/ch12/alien_invasion/alien_invasion.py
+++ python/crash_course/ch12/alien_invasion/alien_invasion.py
@@ -1,9 +1,11 @@
 # alien_invasion
-import sys
+#import sys
 import pygame
+
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	# Initialize game and create a screen object.
@@ -20,17 +22,9 @@ def run_game():
 	
 	# Start the main loop for the game.
 	while True:
-		
-		#Watch for keyboard and mouse events.
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-				
-		# Redraw the screen during each pass through the loop.
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
-		
-		# Make the most recently drawn screen visible.
-		pygame.display.flip()
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(ai_settings, screen, ship)		
+
 		
 run_game()
