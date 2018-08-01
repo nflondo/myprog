982d60c18aaca768a18cd293c16f19d6146f1777
Nefi Munoz <nefi.munoz@gmail.com>
2018-08-01 16:45:56 -0600

    changes

--- python/crash_course/ch12/alien_invasion/alien_invasion.py
+++ python/crash_course/ch12/alien_invasion/alien_invasion.py
@@ -24,7 +24,7 @@ def run_game():
 	# Make an alien
 #	alien = Alien(ai_settings, screen)
 	# Create the fleet of aliens.
-	gf.create_fleet(ai_settings, screen, aliens)
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 		
 	# Start the main loop for the game.
 	while True:
