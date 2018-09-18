fa930e7912bf36d60e98b7903d5e919f005cb74e
Nefi Munoz <nefi.munoz@gmail.com>
2018-09-18 05:53:48 -0600

    changes

---
+++ Java/21days7/src/com/java21days/DiceWorker.java
@@ -0,0 +1,27 @@
+package com.java21days;
+
+import javax.swing.*;
+
+public class DiceWorker extends SwingWorker {
+    int timesToRoll;
+    
+    // set up the Swing worker
+    public DiceWorker(int timesToRoll){
+        super();
+        this.timesToRoll = timesToRoll;
+    }
+    
+    // define the task the worker performs
+    protected int [] doInBackground(){
+        int[] result = new int[16];
+        for (int i = 0; i < this.timesToRoll; i++){
+            int sum = 0;
+            for (int j = 0; j < 3; j++){
+                sum += Math.floor(Math.random() * 6);
+            }
+            result[sum] = result[sum] + 1;    
+        }
+        // transmit the result
+        return result;       
+    }    
+}
