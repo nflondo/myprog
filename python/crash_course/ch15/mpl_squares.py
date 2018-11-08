b97770d31fc5cc5e91fc3896c8870857235e23f2
Nefi Munoz <nefi.munoz@gmail.com>
2018-11-08 16:13:08 -0700

    changes

--- python/crash_course/ch15/mpl_squares.py
+++ python/crash_course/ch15/mpl_squares.py
@@ -1,15 +1,16 @@
 #mpl_squares.py
 import matplotlib.pyplot as plt
-import matplotlib.rcsetup as rcsetup
-print(rcsetup.all_backends)
+#import matplotlib.rcsetup as rcsetup
+#print(rcsetup.all_backends)
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1,4,9,16,25]
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
-plt.ylabel("squre of Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 plt.tick_params(axis='both', labelsize=14)
---
+++ python/crash_course/ch15/scatter_squares.py
@@ -0,0 +1,25 @@
+#scatter_squares.py
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+#x_values = [1, 2, 3, 4, 5]
+#y_values = [1, 4, 9, 16, 25]
+
+#plt.scatter(2, 4, s=200)
+# You can pass the RGB color model.  Values closer to 0 produce dark colors
+# values closer to 1 produce lighter colors.
+plt.scatter(x_values, y_values, c=(0.5, 0, 0.8), edgecolor='none', s=40)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+#plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
+
+plt.show()
