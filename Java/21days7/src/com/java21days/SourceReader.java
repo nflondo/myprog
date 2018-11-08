5ec89c7d7339ca75fcf41afb1028fe0fe5078a88
Nefi Munoz <nefi.munoz@gmail.com>
2018-11-08 05:59:19 -0700

    new file

---
+++ Java/21days7/src/com/java21days/SourceReader.java
@@ -0,0 +1,20 @@
+/*
+ * Sams TeachYourself Java in 21 days all rights reserved.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java21days;
+
+import java.io.*;
+
+public class SourceReader {
+    public static void main(String[] args){
+        try (FileReader file = new FileReader("SourceReader.java");
+                BufferedReader buff = new BufferedReader(file)
+                ){
+                 
+                boolean eof = false;
+            
+            }
+    }
+}
