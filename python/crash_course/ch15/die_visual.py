32e476fb273c412b5399c0b9653ce200e76074e3
Nefi Munoz <nmunoz@suse.com>
2018-12-07 16:20:04 -0700

    changes

--- python/crash_course/ch15/die_visual.py
+++ python/crash_course/ch15/die_visual.py
@@ -6,17 +6,18 @@ from die import Die
 die = Die()
 
 # Make some rolls, and store results in a list.
-results = []
-for roll_num in range(1000):
-	result = die.roll()
-	results.append(result)
+#results = []
+#for roll_num in range(1000):
+#	result = die.roll()
+#	results.append(result)
+results=[ die.roll() for result in range(1000) ]
 
 # Analyze the results.
-frequencies = []
-for value in range(1, die.num_sides+1):
-	frequency = results.count(value)
-	frequencies.append(frequency)
-		
+#frequencies = []
+#for value in range(1, die.num_sides+1):
+#	frequency = results.count(value)
+#	frequencies.append(frequency)
+frequencies = [ results.count(value) for value in range(1, die.num_sides+1) ]		
 #print(results)
 print(frequencies)
 # Visualize the results.
@@ -24,7 +25,8 @@ print(frequencies)
 hist = pygal.Bar()
 
 hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+#hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = [ num for num in range(1, 7) ]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
