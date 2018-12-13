18908f05b01165ddedf2a007a24b131006fab9e0
Nefi Munoz <nmunoz@suse.com>
2018-12-13 16:47:17 -0700

    new files and changes

--- python/crash_course/ch16/world_population.py
+++ python/crash_course/ch16/world_population.py
@@ -1,6 +1,8 @@
 #world_population
 import json
 
+from country_codes import get_country_code
+
 # Load the data into a list.
 filename = 'population_data.json'
 with open(filename) as f:
@@ -11,4 +13,9 @@ for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
 		population = int(float(pop_dict['Value']))
-		print(country_name + ": " + str(population))
+		code = get_country_code(country_name)
+		if code:
+			print(code + ": " + str(population))
+		else:
+			print('ERROR - ' + country_name)
+		
