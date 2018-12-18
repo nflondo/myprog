53bad0fecbcd7ae9a3e77f0994d9d5a65d508d0a
Nefi Munoz <nmunoz@suse.com>
2018-12-18 08:04:56 -0700

    changes

--- python/crash_course/ch16/world_population.py
+++ python/crash_course/ch16/world_population.py
@@ -1,6 +1,7 @@
 #world_population
 import json
 
+from pygal.maps.world import World
 from country_codes import get_country_code
 
 # Load the data into a list.
@@ -8,6 +9,8 @@ filename = 'population_data.json'
 with open(filename) as f:
 	pop_data = json.load(f)
 
+# Build a dictionary of population data
+cc_populations = {}
 # Print the 2010 population for each country.
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
@@ -15,7 +18,14 @@ for pop_dict in pop_data:
 		population = int(float(pop_dict['Value']))
 		code = get_country_code(country_name)
 		if code:
-			print(code + ": " + str(population))
-		else:
-			print('ERROR - ' + country_name)
+			cc_populations[code] = population
+			#print(code + ": " + str(population))
+#		else:
+#			print('ERROR - ' + country_name)
+			
+wm = World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
 		
