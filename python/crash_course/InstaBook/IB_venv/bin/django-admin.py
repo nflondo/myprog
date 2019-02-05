fe4fa1ec4d8ff53c622eda43873fab2bd755d48b
Nefi Munoz <nefi.munoz@gmail.com>
2019-02-05 16:25:29 -0700

    new files

---
+++ python/crash_course/InstaBook/IB_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nmunoz/myprog/python/crash_course/InstaBook/IB_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python/crash_course/learning_log/learning_logs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Topic(models.Model):
+	"""A topic the user is learning about"""
+	text = models.CharField(max_length=200)
+	date_added = models.DatetimeField(auto_now_add=True)
+	
+def __str__(self):
+	"""Return a string representation of the model."""
+	return self.text
+	
