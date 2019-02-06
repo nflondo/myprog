9dd3c7dc0a547ef833798ce82d01f3bd1c6f62a0
Nefi Munoz <nefi.munoz@gmail.com>
2019-02-06 16:25:16 -0700

    changes

--- python/crash_course/learning_log/learning_logs/models.py
+++ python/crash_course/learning_log/learning_logs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Topic(models.Model):
 	"""A topic the user is learning about"""
 	text = models.CharField(max_length=200)
-	date_added = models.DatetimeField(auto_now_add=True)
+	date_added = models.DateTimeField(auto_now_add=True)
 	
 def __str__(self):
 	"""Return a string representation of the model."""
