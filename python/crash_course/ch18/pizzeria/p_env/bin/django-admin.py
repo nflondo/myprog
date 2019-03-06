39d7e0c3977d302bebc8d4f2a8b723c1861f35e4
Nefi Munoz <nefi.munoz@gmail.com>
2019-03-06 08:07:53 -0700

    new files

---
+++ python/crash_course/ch18/pizzeria/p_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nmunoz/myprog/python/crash_course/ch18/pizzeria/p_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
