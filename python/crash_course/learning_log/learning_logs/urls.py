963bb134f5397e8fc2bbe36136bc324c087b43f2
Nefi Munoz <nefi.munoz@gmail.com>
2019-03-25 16:41:13 -0600

    changes

--- python/crash_course/learning_log/learning_logs/urls.py
+++ python/crash_course/learning_log/learning_logs/urls.py
@@ -8,4 +8,7 @@ app_name = 'learning_logs'
 urlpatterns = [
 	# Home page.
 	path('', views.index, name='index'),
+	
+	# Show all topics
+	path('topics/', views.topics, name='topics'),
 ]
---
+++ python/crash_course/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nmunoz/myprog/python/crash_course/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
