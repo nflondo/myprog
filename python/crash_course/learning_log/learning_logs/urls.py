9059dd1fda2fe5dd27713b8cde48fd84e21b5ed9
Nefi Munoz <nefi.munoz@gmail.com>
2019-04-23 16:48:51 -0600

    changes

--- python/crash_course/learning_log/learning_logs/urls.py
+++ python/crash_course/learning_log/learning_logs/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
 	
 	# Page for adding a new topic
 	path('new_topic/', views.new_topic, name='new_topic'),
+	
+	# Page for adding a new entry
+	path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
 ]
