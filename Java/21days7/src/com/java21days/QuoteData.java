acc164e4ebdecbe1f280d810e836b937b2e9834b
Nefi Munoz <nefi.munoz@gmail.com>
2019-06-11 05:55:37 -0600

    changes

--- Java/21days7/src/com/java21days/QuoteData.java
+++ Java/21days7/src/com/java21days/QuoteData.java
@@ -35,4 +35,41 @@ public class QuoteData {
         }
         return builder.toString();
     }
+    
+    private void storeQuote(String data){
+        StringTokenizer tokens = new StringTokenizer(data, ",");
+        String[] fields = new String[9];
+        for (int i = 0; i < fields.length; i++){
+            fields[i] = stripQuotes(tokens.nextToken());
+        }
+        String datasource = "jdbc:derby://localhost:1527/sample";
+        try (Connection conn = DriverManager.getConnection(datasource,"app","app"))
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            PreparedStatement prep2 = conn.prepareStatement (
+                    "insert into " +
+                    "APP.STOCKS(TICKER, PRICE, DATE, CHANGE, LOW, " +
+                    "HIGH, PRICEOPEN, VOLUME) " +
+                    "values(?, ?, ?, ?, ?, ?, ?, ?)");
+            prep2.setString(1, fields[0]);
+            prep2.setString(2, fields[1]);
+            prep2.setString(3, fields[2]);
+            prep2.setString(4, fields[3]);
+            prep2.setString(5, fields[4]);
+            prep2.setString(6, fields[5]);
+            prep2.setString(7, fields[6]);
+            prep2.setString(8, fields[7]);
+            prep2.executeUpdate();
+            prep2.close();
+            conn.close();
+        } catch (SQLException sqe){
+            System.out.println("SQL Error: " + sqe.getMessage());
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println(cnfe.getMessage());
+        }        
+    }
+    
+    private String stripQuotes(String input){
+        
+    }
 }
