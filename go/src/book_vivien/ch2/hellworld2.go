4b3f3a932e514e32c48c7b613cda30165c8a232a
Nefi Munoz <nefi.munoz@gmail.com>
2019-07-11 16:34:32 -0600

    adding ch2

---
+++ go/src/book_vivien/ch2/hellworld2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "math/rand"
+import "time"
+
+var greetings = [][]string{
+	{"Hello, World!", "English"},
+	{"Hola Mundo!", "Spanish"},
+	{"Salut Monde", "French"},
+}
+
+func greeting() [] string {
+	seed := time.Now().UnixNano()
+	rnd := rand.New(rand.NewSource(seed))
+	return greetings[rnd.Intn(len(greetings))]
+}
+
+func main() {
+	g := greeting()
+	fmt.Prinf("%s (%s)\n", g[0], g[1])
+}
