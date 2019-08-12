c6abf5d689c647ade5378c2f9e3756f2d81db219
Nefi Munoz <nmunoz@suse.com>
2019-08-12 16:46:42 -0600

    new files

---
+++ python/online-tutorials/app.py
@@ -0,0 +1,18 @@
+# app.py
+import os,glob
+import sys,time
+import signal
+import re
+import string
+import commands
+program = 'python'
+arguments = ['mod.py']
+studentlist = ['one','two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen']
+#print(os.execvp(program, (program,) + tuple(arguments)))
+print("Executed: "+program + " " + " ".join(arguments))
+#print(os.execvp(program, [program,] + arguments))
+#print(os.execvp(program, [program,] + studentlist))
+
+#cmdline=['python']
+#cmdargs = ['myarg1','myarg2','myarg3']
+#print(os.execvp(cmdline[0], [ cmdline[0], ]+cmdargs))
---
+++ python/online-tutorials/mod.py
@@ -0,0 +1,10 @@
+#mod.py
+
+student = {
+	'name': 'Krunal',
+	'enno': 21,
+	'college': 'byu'
+	}
+studentlist = ['one','two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen']
+#print(student)
+print(studentlist)
