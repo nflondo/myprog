e189628755cabc7e38fa1e79974042c23716451a
Nefi Munoz <nefi.munoz@gmail.com>
2019-12-09 16:43:35 -0700

    all files

--- ruby/safari/project_euler_5.rb
+++ ruby/safari/project_euler_5.rb
@@ -1,2 +1,4 @@
 #https://projecteuler.net/problem=5
+# .reduce(:lcm) - applies the operation to all elements in the array.  Same as
+# inject
 p (1..20).to_a.reduce(:lcm) #lcm = least common multiple
