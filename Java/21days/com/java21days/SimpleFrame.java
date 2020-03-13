39aea3407a90f68909e3769606cd54c829642a66
Nefi Munoz <nmunoz@suse.com>
2020-03-13 08:10:21 -0600

    changes

--- Java/21days/com/java21days/SimpleFrame.java
+++ Java/21days/com/java21days/SimpleFrame.java
@@ -1,5 +1,29 @@
 package com.java21days;
 
-public class SimpleFrame {
+import javax.swing.*;
 
+public class SimpleFrame extends JFrame {
+	
+	public SimpleFrame() {
+		super("Frame Title");
+		setSize(300,100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLookAndFeel();
+		setVisible(true);
+	}
+	
+	private static void setLookAndFeel() {
+		try {
+			UIManager.setLookAndFeel(
+					"com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+			);
+		} catch (Exception exc) {
+			// ignore error
+		}
+	}
+	
+	public static void main(String[] args) {
+		setLookAndFeel();
+		SimpleFrame sf = new SimpleFrame();
+	}
 }
