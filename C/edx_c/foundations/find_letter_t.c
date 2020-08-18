8413944cfff7e3c926a3ba8600947865fe570c90
Nefi Munoz <nmunoz@suse.com>
2020-08-18 17:01:04 -0600

    changes

--- C/edx_c/foundations/find_letter_t.c
+++ C/edx_c/foundations/find_letter_t.c
@@ -1,3 +1,10 @@
+/* You are conducting a linguistic study and are interested in finding words 
+that contain the letter 't' or 'T' in the first half of the word (including 
+the middle letter if there is one). Specifically, if the first half of the 
+word does contain a 't' or a 'T', your program should output a 1. If the first 
+half does not contain the letter 't' or 'T', but the second half does, then 
+your program should output a 2. Otherwise, if there is no 't' or 'T' in the word at all, your program's output should be -1. You may assume that the word entered does not have more than 50 letters.
+*/
 #include <stdio.h>
 int main(void) {
     char myWord[51];
@@ -17,7 +24,12 @@ int main(void) {
     i = 0;
     while ( !found && myWord[i] != '\0'){
         if ( myWord[i] == 't'  || myWord[i] == 'T'){
-            found = 1;
+            if (i <= wordLength / 2){
+                found = 1;
+            } else
+            {
+                found = 2;
+            }      
             break;
         } else
         {
