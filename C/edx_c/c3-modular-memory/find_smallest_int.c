2071de6db286efdba6cec37bc7323696b2156b2a
Nefi Munoz <nmunoz@suse.com>
2020-08-26 17:06:54 -0600

    changes

--- C/edx_c/c3-modular-memory/find_smallest_int.c
+++ C/edx_c/c3-modular-memory/find_smallest_int.c
@@ -1,4 +1,8 @@
-/* write a fun
+/* The goal of this problem is to find the smallest integer in a list of numbers.
+
+To help you with this task, please write a function called min() that finds and returns the smallest amongst two integers (be sure to also write a prototype for this function). The function thus takes two integers as input and returns the smallest of the two. This function will use an if statement with a condition that contains either "less than" or "greater than".
+
+Next, please use min() in your main function to work your way through an entire list of numbers in order to find its minimum. The first number you read gives the number of elements in the list of integers under consideration. You will then read the integer values, using min() to keep only the smallest integer read at each step. In the end, please print out the smallest integer in the list.
 */
 #include <stdio.h>
 int min( int x, int y);
@@ -10,17 +14,18 @@ int main(void) {
     int numList[numElements + 1];
     int listValue, currentMin, smallestNum;
 
-    for(int i = 0; i < numElements; i++){
+    //Read first two elements outside of the loop
+    scanf("%d %d", &numList[0], &numList[1]);
+    smallestNum = min(numList[0], numList[1]);        
+
+    for(int i = 2; i < numElements; i++){
         scanf("%d", &listValue);
         numList[i] = listValue;
         currentMin = min(numList[i], numList[i - 1]);
-        if(i == 0){
-            smallestNum = currentMin;
-        } else if ( currentMin < smallestNum )
+        if ( currentMin < smallestNum )
         {
             smallestNum = currentMin;
-        }
-        
+        }        
     }
 
     printf("%d\n", smallestNum);
