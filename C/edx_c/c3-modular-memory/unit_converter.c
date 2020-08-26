319dfed953c44153f561e84c1322fb7d9a060640
Nefi Munoz <nmunoz@suse.com>
2020-08-26 05:21:51 -0600

    changes

--- C/edx_c/c3-modular-memory/unit_converter.c
+++ C/edx_c/c3-modular-memory/unit_converter.c
@@ -5,6 +5,8 @@ temperature in degrees Fahrenheit = 32 + 1.8 × temperature in degrees Celsius.
 #include <stdio.h>
 
 double metersToFeet( double metricNum );
+double gramsToPounds( double metricAmount);
+double celciusToFarenheit ( double metricInput);
 
 int main(void) {
     int numConversions;
@@ -12,11 +14,22 @@ int main(void) {
     char metricUnit;
     scanf("%d", &numConversions);
 
-    scanf("%lf %c", &metricInput, &metricUnit );
-    if(metricUnit == 'm'){
-        double feet;
-        feet = metersToFeet( metricInput);
-        printf("%6lf ft", feet);
+    for(int i = 0 ; i < numConversions; i++){
+        scanf("%lf %c", &metricInput, &metricUnit );
+        if(metricUnit == 'm'){
+            double feet;
+            feet = metersToFeet( metricInput);
+            printf("%6lf ft\n", feet);
+        } else if (metricUnit == 'g')
+        {
+            double pounds;
+            pounds = gramsToPounds( metricInput);
+            printf("%6lf lbs\n", pounds);
+        } else if (metricUnit == 'c'){
+            double farenheit;
+            farenheit = celciusToFarenheit( metricInput);
+            printf("%6lf f\n", farenheit);
+        }
     }
 
 //    printf("You entered: %d %c\n", metricInput, metricUnit);
@@ -27,5 +40,17 @@ double metersToFeet( double metricNum ){
     double result;
 
     result = metricNum * 3.2808;
+    return result;
+}
+
+double gramsToPounds( double metricAmount){
+    double result;
+    result = metricAmount * 0.002205 ;
+    return result;
+}
+
+double celciusToFarenheit ( double metricInput){
+    double result;
+    result = (metricInput * 1.8) + 32;
     return result;
 }
