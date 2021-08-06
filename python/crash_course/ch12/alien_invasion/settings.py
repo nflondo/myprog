57170668400dc98df96625998ecbbf1d2c4bd86f
Nefi Munoz <nefi.munoz@gmail.com>
2021-08-06 16:08:25 -0600

    new settings for alien_invasion

--- python/crash_course/ch12/alien_invasion/settings.py
+++ python/crash_course/ch12/alien_invasion/settings.py
@@ -10,7 +10,7 @@ class Settings():
 				
 		# Ship settings
 		self.ship_speed_factor = 1.5
-		self.ship_limit = 2
+		self.ship_limit = 3
 		
 		# Bullet Settings
 		self.bullet_speed_factor = 1
@@ -21,7 +21,7 @@ class Settings():
 
 		# Alien Settings
 		self.alien_speed_factor = 1
-		self.fleet_drop_speed = 10
+		self.fleet_drop_speed = 7
 		# fleet_direction of 1 represents right; -1 represents left.
 		self.fleet_direction = 1
 		
