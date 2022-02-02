0a99b1f749884879279ea104cdd7b9a71999fd70
Nefi Munoz Calderon <nefi.munoz@gmail.com>
2022-02-01 19:35:17 -0700

    new c++ files

---
+++ c++/in-project/hello.cpp
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
+ */
+// hello.cpp by Bill Weinman [bw.org]
+// updated 2020-06-24
+#include <cstdio>
+
+int main()
+{
+    puts("Hello, World!");
+    puts("Hello, World Again!");
+    return 0;
+}
