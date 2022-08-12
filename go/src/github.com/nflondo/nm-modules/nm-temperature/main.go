468c5914292c69849dbbb9e428ae000d3c0fe0a4
Nefi Munoz <nefi.munoz@gmail.com>
2022-08-11 21:39:43 -0600

    new changes to go files

--- go/src/github.com/nflondo/nm-modules/nm-temperature/main.go
+++ go/src/github.com/nflondo/nm-modules/nm-temperature/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/shapeshed/temperature"
+	temperature "github.com/nflondo/nm-modules/nm-temperature"
 )
 
 func main() {
 	f := temperature.CtoF(10)
-	fmt.Println(f) // 10
+	fmt.Println(f) // 50
 
 	c := temperature.FtoC(32)
 	fmt.Println(c) // 0
+
+	k := temperature.CtoK(-253.150)
+	fmt.Println(k) // 0
 }
--- go/src/github.com/nflondo/nm-modules/nm-temperature/temperature.go
+++ go/src/github.com/nflondo/nm-modules/nm-temperature/temperature.go
@@ -9,3 +9,8 @@ func CtoF(c float64) Temperature {
 func FtoC(f float64) Temperature {
 	return Temperature((f - 32) * 5 / 9)
 }
+
+// Formula to convert celsius to Kelvins  T (K) = T (°C) + 273.15.
+func CtoK(c float64) Temperature {
+	return Temperature(c + 273.15)
+}
